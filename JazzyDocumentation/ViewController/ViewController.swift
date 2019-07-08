d5e8472ddfa9d6e8fbb1e8d231a3a725bef4ee0b
Sumit, Kumar <sumit16.kumar@gmail.com>
2019-07-08 15:06:58 +0530

    Development team details added

--- JazzyDocumentation/ViewController/ViewController.swift
+++ JazzyDocumentation/ViewController/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     /**
      Example project to demonstrate the documentation generation using Jazzy
      Add `.jazzy.yaml` to project doc and type `jazzy` from command terminal, document will be generated
-     *Follow this link for details*
+     **Follow this link for details**
      https://medium.com/@sumit16.kumar/document-generation-for-swift-projects-using-jazzy-e148efc72bca
      */
     func documentGenerationUsingJazzy() {
